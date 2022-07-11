668494ca6fb00d4852d9888e16b0e06218b68190
umaretiya <umaretiya@gmail.com>
2022-07-12 00:39:09 +0530

    Project frame work

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MLops_classifcation
+
+
+[repo link]("https://github.com/umaretiya/MLops_classifcation")
+
+Create first flask classifciation  machine learning project
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
-"https://github.com/umaretiya/MLops_classifcation"
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to Machine Learning Classification Automation Projects</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
